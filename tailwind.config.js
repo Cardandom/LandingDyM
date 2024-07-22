1694729c28fe3d71022638fcd5e5a5caf11b0bec
Carlos Daniel Dominguez <carlosddominguezc@gmail.com>
2024-07-22 11:45:48 -0500

    Configurated Layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,13 +24,13 @@ module.exports = {
       colors: {
         primary: '#131424',
         secondary: '#393A47',
-        accent: '#F13024',
+        accent: '#FF8BD2',
       },
       backgroundImage: {
         explosion: 'url("/bg-explosion.png")',
         circles: 'url("/bg-circles.png")',
         circleStar: 'url("/circle-star.svg")',
-        site: 'url("/site-bg.svg")',
+        // site: 'url("/site-bg.svg")',
       },
       animation: {
         'spin-slow': 'spin 6s linear infinite',
